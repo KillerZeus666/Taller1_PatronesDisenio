c60197ee2179ab2e7a05683c4c9896fb42e07aa9
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-10 13:35:27 +0000

    Agregando clase comparadorCita

---
+++ barberia/src/main/java/com/patrones/barberia/ComparadorCita.java
@@ -0,0 +1,14 @@
+package com.patrones.barberia; 
+
+
+import java.time.LocalTime;
+import java.util.Comparator;
+
+public class ComparadorCita implements Comparator<Cita> {
+    @Override
+    public int compare(Cita c1, Cita c2) {
+        LocalTime t1 = LocalTime.parse(c1.getHora());
+        LocalTime t2 = LocalTime.parse(c2.getHora());
+        return t1.compareTo(t2);
+    }
+}
