341003f4560bb73bb87877c1d868cd881eed8b28
Katheryn Guasca <katheryng2017@gmail.com>
2025-02-10 13:43:37 +0000

    Agregando clase test

--- barberia/src/test/java/com/patrones/barberia/ControladorCitasTest.java
+++ barberia/src/test/java/com/patrones/barberia/ControladorCitasTest.java
@@ -1,13 +1,13 @@
-package com.patrones.barberia;
+package com.patrones.barberia; 
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BarberiaApplicationTests {
+class ControladorCitasTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
